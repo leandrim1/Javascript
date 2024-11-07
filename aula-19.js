ed74927c7d60f613bb6d289f05b0ac10d4a87725
leandrim1 <leandrimrodrigues532@gmail.com>
2024-11-07 14:23:36 -0300

    Javascript

---
+++ aula-19.js
@@ -0,0 +1,18 @@
+function teste(key) {
+    switch (key) {
+        case 'Julio':
+            console.log('JULIO AQUI');
+            break;
+        case 'Pedro':
+            console.log('PEDRO AQUI');
+            break;
+        case 'Joao':
+            console.log('JOAO AQUI');
+            break;
+        default:
+            console.log('QUALQUER OUTRO');
+            break;
+    }
+}
+
+teste('dpawkfp')
