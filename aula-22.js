2404aa799e40cd270697edd2cb1f629ce48752f2
leandrim1 <leandrimrodrigues532@gmail.com>
2024-11-07 15:22:39 -0300

    Javascript

---
+++ aula-22.js
@@ -0,0 +1,13 @@
+const idadePai = 41;
+
+const exibirIdade = () => {
+    const idadeFilho = 19;
+    console.log(idadeFilho);
+}
+
+const exibirExibirIdade = () => {
+    exibirIdade();
+    console.log(idadePai);
+}
+
+exibirExibirIdade()
