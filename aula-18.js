05bea054a2d7bb54a8842a81009ad7507f3ac2ef
leandrim1 <leandrimrodrigues532@gmail.com>
2024-11-07 14:06:47 -0300

    Javascript

---
+++ aula-18.js
@@ -0,0 +1,12 @@
+const nmero = 3244;
+const stringA = 'dafivnona'; // Imutavel pois não modifica.
+const verdadeOuFalso = true; // false
+
+const arrayTest = ['daew','daea','dapf'];
+
+stringA[4] = 'JULIO';
+
+arrayTest.push('dafe');
+
+console.log(arrayTest);
+console.log(stringA);
