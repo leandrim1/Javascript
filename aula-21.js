20fee2e71cf8a6622e4fe9656e76a41b4eb19f00
leandrim1 <leandrimrodrigues532@gmail.com>
2024-11-07 15:06:31 -0300

    Javascript

---
+++ aula-21.js
@@ -0,0 +1,18 @@
+function calcular(a,b) {
+    console.log('calcular')
+}
+
+const calcular2 = function() {
+    console.log('calcular 2')
+}
+
+const calcular3 = () => {
+    console.log('calcular 3')
+}
+
+
+calcular();
+calcular2();
+calcular3();
+
+// Formas de criar uma função.
