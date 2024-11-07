3c40c897f0955f41093f5be96a92e41c279b6d95
leandrim1 <leandrimrodrigues532@gmail.com>
2024-11-07 14:47:48 -0300

    Javascript

--- aula-18.js
+++ aula-18.js
@@ -1,12 +1,16 @@
-const numero = 3244;
 const stringA = 'dafivnona'; // Imutavel pois não modifica.
 const verdadeOuFalso = true; // false
 
+// Arrays são mutáveis, ou seja, podemos alterar seu conteúdo.
 const arrayTest = ['daew','daea','dapf'];
 
-stringA[4] = 'JULIO';
+// Tentativa de modificar um caractere específico de uma string
+stringA[4] = 'JULIO'; // Isso não funcionará pois não altera a stringA.
 
+// O valor 'dafe' será adicionado ao final do array arrayTest
 arrayTest.push('dafe');
 
+// Resultado: ['daew', 'daea', 'dapf', 'dafe']
 console.log(arrayTest);
+// Resultado: 'dafivnona' (não foi alterada)
 console.log(stringA);
