d68f396f1ada48645baff2e2f9bf6a56fd3a2fa9
leandrim1 <leandrimrodrigues532@gmail.com>
2024-11-08 16:13:33 -0300

    Javascript

---
+++ aula-26.js
@@ -0,0 +1,20 @@
+process.stdin.setEncoding('utf8');
+
+const soma = (valor) => {
+    if ( valor < 0 || valor > 100) {
+        throw new Error ('Valor invalido, programa será encerrado')
+    }
+    if (valor === 0) {
+        return 0;
+    }
+    return valor + soma(valor - 1)
+}
+
+process.stdin.on('data', function(data) {
+    try {
+        console.log('soma:', soma(Number(data)))
+    } catch (error) {
+        console.log(error.message)
+        process.stdin.pause()
+    }
+});
