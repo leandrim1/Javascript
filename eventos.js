ebd562a362339a2eae0b850ae7b36148a4765abb
leandrim1 <leandrimrodrigues532@gmail.com>
2024-12-17 16:05:00 -0300

    javascript

---
+++ eventos.js
@@ -0,0 +1,21 @@
+// EVENTOS DE MOUSE
+
+onclick // Quando o usuário clica em um elemento.
+ondblclick // Quando clica duas vezes em um elemento.
+onmousedown // Pressiona o botão sobre um elemento.
+onmouseover // Ponteiro do mouse está sobre um elemento.
+onmouseout // Ponteiro do mouse é movido para fora de um elemento.
+onmouseup // Botão do mouse é solto sobre um elemento.
+
+// EVENTOS DE TECLADO
+
+onkeydown // O usuário está pressionando uma tecla.
+onkeypress // O usuário pressiona uma tecla.
+onkeyup // O usuário solta uma tecla (previamento pressionada).
+
+// EVENTOS MOBILE
+
+ontouchcancel // Ocorre quando o toque é interrompido.
+ontouchend // Ocorre quando o dedo é removido da tela.
+ontouchmove // Ocorre quando o dedo se move na tela.
+ontouchstart // Ocorre quando o dedo entra em contato com a tela.
